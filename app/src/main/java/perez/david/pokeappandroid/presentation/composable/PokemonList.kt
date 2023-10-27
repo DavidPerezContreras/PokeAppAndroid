a45c4726d790bed58396155332a318ca0819eb04
DavidPerezContreras <david.perez.dam2@gmail.com>
2023-10-27 09:42:34 +0200

    LoadingIndicator abajo.

--- app/src/main/java/perez/david/pokeappandroid/presentation/composable/PokemonList.kt
+++ app/src/main/java/perez/david/pokeappandroid/presentation/composable/PokemonList.kt
@@ -1,6 +1,8 @@
+
 package perez.david.pokeappandroid.presentation.composable
 
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,6 +10,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -15,38 +18,44 @@ import androidx.paging.compose.itemKey
 import perez.david.pokeappandroid.presentation.viewmodel.PokemonListViewModel
 import java.util.UUID
 
-
 @Composable
 fun PokemonList(viewModel: PokemonListViewModel) {
     val pokemons = viewModel.allPokemonFlow.collectAsLazyPagingItems()
 
+    val isRefreshing = pokemons.loadState.refresh is LoadState.Loading
+    val isAppending = pokemons.loadState.append is LoadState.Loading
+
     Scaffold { paddingValues ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ) {
-            items(
-                count = pokemons.itemCount,
-                key = pokemons.itemKey { pokemon -> UUID.randomUUID().toString() }
-            ) { characterIndex ->
-                pokemons[characterIndex]?.let { item ->
-                    PokemonCard(pokemon = item)
+            if(pokemons.itemCount<=0){
+                Box(modifier = Modifier.fillMaxSize(),contentAlignment = Alignment.BottomCenter) {
+                    LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
                 }
             }
-        }
-        // Show loading indicator for both refresh and append
-        val isRefreshing = pokemons.loadState.refresh is LoadState.Loading
-        val isAppending = pokemons.loadState.append is LoadState.Loading
 
-        if (isRefreshing || isAppending) {
-            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
-        }
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+                items(
+                    count = pokemons.itemCount,
+                    key = pokemons.itemKey { _ -> UUID.randomUUID().toString() }
+                ) { characterIndex ->
+                    pokemons[characterIndex]?.let { item ->
+                        PokemonCard(pokemon = item)
+                    }
+
+                    // Check if we're at the last item in the list
+                    if (characterIndex == pokemons.itemCount - 1) {
+                        // Show the LinearProgressIndicator at the bottom
 
 
-
+                        if (isRefreshing || isAppending) {
+                            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+                        }
+                    }
+                }
+            }
 
     }
 }
-
-
