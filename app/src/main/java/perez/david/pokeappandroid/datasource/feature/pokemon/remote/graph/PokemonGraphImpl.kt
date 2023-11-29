b6fba6efcb8798855af076c6cb32d6f49dbb24d3
DavidPerezContreras <david.perez.dam2@gmail.com>
2023-11-29 18:53:52 +0100

    Unused Imports

--- app/src/main/java/perez/david/pokeappandroid/datasource/feature/pokemon/remote/graph/PokemonGraphImpl.kt
+++ app/src/main/java/perez/david/pokeappandroid/datasource/feature/pokemon/remote/graph/PokemonGraphImpl.kt
@@ -1,8 +1,5 @@
 package perez.david.pokeappandroid.datasource.feature.pokemon.remote.graph
 
-import com.apollographql.apollo.api.create
-import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.cache.normalized.RecordFieldJsonAdapter.Companion.create
 import perez.david.pokeappandroid.datasource.feature.pokemon.remote.RemoteImpl
 import perez.david.pokeappandroid.model.Pokemon
 import javax.inject.Inject
